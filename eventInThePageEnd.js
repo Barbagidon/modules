8c75a6b4004ed817d1ac016e436257376dfa18c8
Barbagidon <77959708+Barbagidon@users.noreply.github.com>
2022-03-05 14:23:27 +0300

    Update eventInThePageEnd.js

--- eventInThePageEnd.js
+++ eventInThePageEnd.js
@@ -1,6 +1,7 @@
  function openByScroll(selector) {
         window.addEventListener('scroll', () => {
             if (!btnPressed && (window.pageYOffset + document.documentElement.clientHeight == document.documentElement.scrollHeight)) {
+             document.querySelector(selector).click();
 
 
             }
