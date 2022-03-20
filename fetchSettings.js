a9b852c31f9220838ac4777d100ac1fea27ed95f
Barbagidon <77959708+Barbagidon@users.noreply.github.com>
2022-03-20 13:56:36 +0300

    Create fetchSettings.js

---
+++ fetchSettings.js
@@ -0,0 +1,7 @@
+const postData = async (url, data) => {
+        let res = await fetch(url, {
+            method: 'POST',
+            body: data,
+        });
+        return await res.text();
+    };
