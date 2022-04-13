d54174b848d8dd4a5ee8d2f0f90dc4a3297f8637
Barbagidon <77959708+Barbagidon@users.noreply.github.com>
2022-04-13 14:42:02 +0300

    Update onlyRussianInput.js

--- onlyRussianInput.js
+++ onlyRussianInput.js
@@ -2,10 +2,10 @@ const checkTextInputs = (selector) => {
     const inputs = document.querySelectorAll(selector);
 
     inputs.forEach(input => {
-        input.addEventListener('keypress', (e) => {
-            if(e.key.match(/[^а-яё0-9]/)){
-                e.preventDefault();
-
+        input.addEventListener('input', (e) => {
+            if (input.value.match(/[^а-яё0-9]/)) {
+                input.value = "";
+                input.blur(); 
             }
 
         });
