83ce835ce2155651fff04c07e873cfdee7fc5644
Barbagidon <77959708+Barbagidon@users.noreply.github.com>
2022-04-24 15:27:49 +0300

    Update onlyRussianInput.js

--- onlyRussianInput.js
+++ onlyRussianInput.js
@@ -1,17 +1,4 @@
-const checkTextInputs = (selector) => {
-    const inputs = document.querySelectorAll(selector);
-
-    inputs.forEach(input => {
-        input.addEventListener('input', (e) => {
-            if (input.value.match(/[^а-яё0-9]/)) {
-                input.value = "";
-                input.blur(); 
-            }
-
-        });
-
-    });
-
-
-
-};
+if (input.value.match(/[^а-яё0-9]/)) {
+input.value = "";
+input.blur();
+}
