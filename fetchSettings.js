d44c3e14eb7f3caa584ddc2daae5d6d284ffe4d1
Barbagidon <77959708+Barbagidon@users.noreply.github.com>
2022-06-13 14:54:38 +0300

    Update fetchSettings.js

--- fetchSettings.js
+++ fetchSettings.js
@@ -5,3 +5,18 @@ const postData = async (url, data) => {
         });
         return await res.text();
     };
+
+
+
+///get request:
+
+
+
+getData = async (url, data) => {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}. Error: ${res.status}`);
+    }
+    return await res.text();
+  };
